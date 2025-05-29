162323326850c1840a07efee1cb46a20b87b7ad2
theblacksnow95 <enrique.murillo24@gmail.com>
2025-05-29 13:43:11 +0200

    practice

--- powerset/7_practice.c
+++ powerset/7_practice.c
@@ -1,41 +1,64 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   6_practice.c                                       :+:      :+:    :+:   */
+/*   7_practice.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: emurillo <emurillo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/05/23 15:11:23 by emurillo          #+#    #+#             */
-/*   Updated: 2025/05/26 15:12:13 by emurillo         ###   ########.fr       */
+/*   Created: 2025/05/26 13:48:37 by emurillo          #+#    #+#             */
+/*   Updated: 2025/05/26 15:22:30 by emurillo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-int	is_numeric(char *str)
+int	is_numeric(char *s)
 {
-	int i;
+	int	i;
 
 	i = 0;
-	while (str[i])
+	while (s[i])
 	{
-		if (str[i] < '0' || str[i] > '9')
+		if  (s[i] < '0' || s[i] > '9')
 			return (0);
 		i++;
 	}
 	return (1);
 }
 
+int	check_dups(int *set, int len)
+{
+	int	i;
+	int	j;
+	int	n;
+	int count;
 
-int	*copy_set(int *set, int len)
+	i = 0;
+	while (i < len)
+	{
+		i++;
+		j = 0;
+		n = set[i];
+		count = 0;
+		while (set[j])
+		{
+			if (n == set[j])
+				count++;
+			if (count > 1)
+				return (0);
+			j++;
+		}
+	}
+	return (1);
+}
+
+int	*copy_arr(int *set, int len)
 {
 	int	i;
 	int *copy;
 
 	i = 0;
-	if (!set || len < 1)
-		return (NULL);
 	copy = malloc(sizeof(int) * len);
 	if (!copy)
 		return (NULL);
@@ -47,18 +70,18 @@ int	*copy_set(int *set, int len)
 	return (copy);
 }
 
-void sort_set(int *set, int len)
+void	sort_arr(int *set, int len)
 {
 	int	i;
-	int	swapped;
 	int	tmp;
+	int swapped;
 
 	swapped = 1;
 	while (swapped)
 	{
 		swapped = 0;
 		i = 0;
-		while (i < len -1)
+		while (i < len - 1)
 		{
 			if (set[i] > set[i + 1])
 			{
@@ -72,16 +95,16 @@ void sort_set(int *set, int len)
 	}
 }
 
-void	print_set(int *subset, int len)
+void	print_set(int *set, int len)
 {
 	int	i;
-	int *copy;
+	int	*copy;
 
-	copy = copy_set(subset, len);
-	if (copy == NULL)
-		return ;
-	sort_set(copy, len);
 	i = 0;
+	copy = copy_arr(set, len);
+	if (!copy)
+		return;
+	sort_arr(copy, len);
 	while (i < len)
 	{
 		printf("%d", copy[i]);
@@ -102,70 +125,40 @@ void	powerset(int *set, int n, int len, int sum, int idx, int *subset, int sub_l
 	}
 	if (sum > n || idx >= len)
 		return ;
-	subset[sub_len] = set[idx];
+	subset[sub_len] =  set[idx];
 	powerset(set, n, len, sum + set[idx], idx + 1, subset, sub_len + 1);
 	powerset(set, n, len, sum, idx + 1, subset, sub_len);
 }
 
-
-int	check_duplicates(int *set, int len)
+int	main(int ac, char **av)
 {
-	int	i;
-	int	j;
 	int	n;
-	int	count;
-
-	i = 0;
-	while (i < len)
-	{
-		count = 0;
-		n = set[i];
-		j = 0;
-		while (set[j])
-		{
-			if (set[j] == n)
-			{
-				count++;
-			}
-			j++;
-			if (count > 1)
-				return (0);
-		}
-		i++;
-	}
-	return (1);
-}
-
-int main(int ac, char **av)
-{
-	int n;
-	int i;
-	int *set;
-	int *subset;
-	int len;
+	int	i;
+	int	len;
+	int	*set;
+	int	*subset;
 
 	if (ac < 4 || !av[1])
-		return (printf("Error: incorrect arguments.\n"), 2);
+		return (printf("Error: Invalid args.\n"), 1);
 	if (!is_numeric(av[1]))
-		return (printf("Error: (n) must be numeric"), 2);
+		return (printf("Error: (n) must be numeric.\n"), 1);
 	n = atoi(av[1]);
 	len = ac - 2;
+	i = 0;
 	set = malloc(sizeof(int) * len);
 	subset = malloc(sizeof(int) * len);
 	if (!set || !subset)
-		return (printf("Error: allocation error.\n"), 1);
-	i = 0;
+		return(printf("Error: malloc allocations.\n"), 1);
 	while (i < len)
 	{
 		if (!is_numeric(av[i + 2]))
-			return (printf("Error: set must be numeric.\n"), 2);
+			return (printf("Error: set must be numeric.\n"), 1);
 		set[i] = atoi(av[i + 2]);
 		i++;
 	}
-	if (!check_duplicates(set, len))
-		return (printf("Error: no duplicates allowed in set.\n"), 2);
+	if (!check_dups(set, len))
+		return (printf("Error: no duplicates allowed.\n"), 2);
 	powerset(set, n, len, 0, 0, subset, 0);
-	free(set);
 	free(subset);
 	return (0);
 }
